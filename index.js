93b769797ab17ed30abf5f6fd9df23e1667de655
Scarlett Patton <scarwars05@gmail.com>
2018-06-20 20:26:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Write your code in this file!
 
+const currentUser = 'Scarlett Patton';
+
+const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//const firstInitial = 
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
