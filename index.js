a7259d9dff44de302560a15a89be4f06d0814f02
MiaRene15 <kids.hines@icloud.com>
2018-06-20 21:11:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Write your code in this file!
 
+let currentUser = "Mia"
+
+let welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+let shortGreeting = 'Welcome, ' + currentUser.charAt(0) + "!"
+
